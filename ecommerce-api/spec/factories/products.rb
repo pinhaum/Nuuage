2dd22824fea7e6b2458da5134be76af0785e2dbb
Gabriel Ramos <gcrepho@gmail.com>
2020-11-17 16:20:20 -0300

    add Coupon model and tests

--- ecommerce-api/spec/factories/products.rb
+++ ecommerce-api/spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    sequence(:name) { |n| "Product  #{n}" }    
+    sequence(:name) { |n| "Product  #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 1.0..200.0) }
 
