863d87458ffbe36ddf9ac34df1ec8d919eabf5d7
Gabriel Ramos <gcrepho@gmail.com>
2020-11-19 15:22:20 -0300

    add image field to Product model

--- ecommerce-api/spec/factories/products.rb
+++ ecommerce-api/spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "Product  #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 1.0..200.0) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
 
     after :build do |product|
       product.productable = create(:game)
