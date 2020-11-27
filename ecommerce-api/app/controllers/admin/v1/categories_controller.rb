7c8e6766ff59bc2b1e3c04e989c53eff14a43e45
Gabriel Ramos <gcrepho@gmail.com>
2020-11-27 14:27:50 -0300

    add Category update tests

--- ecommerce-api/app/controllers/admin/v1/categories_controller.rb
+++ ecommerce-api/app/controllers/admin/v1/categories_controller.rb
@@ -10,12 +10,18 @@ module Admin::V1
       save_category!
     end
 
+    def update
+      @category = Category.find(params[:id])
+      @category.attributes = category_params
+      save_category!
+    end
+
     private
 
     def category_params
       return {} unless params.has_key?(:category)
 
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:id, :name)
     end
 
     def save_category!
